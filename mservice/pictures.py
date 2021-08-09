490ff10bc268cc765c9e88a1562d4fab10886216
abu <abuhurairah6@yahoo.com>
2021-08-09 23:32:04 +0800

    Add uploading zipfile feature

--- mservice/pictures.py
+++ mservice/pictures.py
@@ -1,7 +1,41 @@
 import base64
 import random
+import zipfile
 from flask import Blueprint, request, send_file, jsonify, make_response
 from mservice.db import db_open
+from werkzeug.datastructures import FileStorage
+
+def get_file_extension(filename):
+	return filename.rsplit('.', 1)[1].lower()
+
+def process_regular_file(file):
+	result = {}
+	file_encode = base64.b64encode(file.read())
+	hash = random.getrandbits(128)
+	link = str("%032x" % hash)
+
+	db = db_open()
+	db.execute(
+		"INSERT INTO PICTURES (P_LINK, P_FILENAME, P_SOURCE) VALUES (?, ?, ?)",
+		(link, file.filename, file_encode)
+	)
+
+	db.commit()
+	result[file.filename] = link
+
+	return result
+
+def process_zip_file(file):
+	links = {}
+
+	with zipfile.ZipFile(file) as zip_object:
+		for filename in zip_object.namelist():
+			if get_file_extension(filename) in ("png", "jpeg", "jpg", "svg", "webp"):
+				curr_file = FileStorage(stream = zip_object.open(filename), filename = filename)
+				link = process_regular_file(curr_file)
+				links.update(link)
+
+	return links
 
 bp = Blueprint("pictures", __name__, url_prefix = "/pictures")
 
@@ -14,27 +48,24 @@ def upload():
 		}
 
 	file = request.files['file']
-	file_encode = base64.b64encode(file.read())
-	hash = random.getrandbits(128)
-	link = str("%032x" % hash)
+	file_ext = get_file_extension(file.filename)
 
-	db = db_open()
-	db.execute(
-		"INSERT INTO PICTURES (P_LINK, P_FILENAME, P_SOURCE) VALUES (?, ?, ?)",
-		(link, file.filename, file_encode)
-	)
-
-	db_cnt = db.execute(
-		"SELECT COUNT(*) FROM PICTURES"
-	).fetchone()
-
-	db.commit()
-	
-	msg = "Successfully uploaded. Your reference link is: " + link
+	if file_ext in ("zip"):
+		links = process_zip_file(file)
+		msg = "Successfully uploaded zip file."
+	elif file_ext in ("png", "jpeg", "jpg", "svg", "webp"):
+		links = process_regular_file(file)
+		msg = "Successfully uploaded " + file_ext + " file."
+	else:
+		return {
+			"status": 0,
+			"msg": "File extension not allowed"
+		}
 
 	return {
 		"status": 1,
-		"msg": msg
+		"msg": msg,
+		"links": links
 	}
 
 @bp.route("/load", methods = ["GET"])
--- tests/test_pictures.py
+++ tests/test_pictures.py
@@ -1,10 +1,12 @@
 import sys
 import io
 import os
+import tempfile
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
 import json
 import base64
+import zipfile
 from mservice.pictures import upload, download
 
 def test_upload(client):
@@ -14,6 +16,33 @@ def test_upload(client):
 	response = response.data.decode('utf8')
 	response = json.loads(response)
 	assert response["status"] == 1
+	assert "Successfully uploaded jpg file" in response["msg"]
+
+def test_upload_zip(client):
+	filefd, filepath = tempfile.mkstemp()
+	zip_object = zipfile.ZipFile(filepath + "temp.zip", "w", zipfile.ZIP_DEFLATED)
+	for x in range(1):
+		zip_object.writestr("test.jpg", io.BytesIO(b"abcdef").getvalue())
+	zip_object.close()
+	zip_object = open(filepath + "temp.zip", "rb")
+	data = {}
+	data['file'] = zip_object
+	response = client.post('/pictures/save', data = data, content_type = 'multipart/form-data')
+	response = response.data.decode('utf8')
+	response = json.loads(response)
+	os.close(filefd)
+	os.unlink(filepath)
+	assert response["status"] == 1
+	assert "Successfully uploaded zip file" in response["msg"]
+
+def test_upload_disallowed_extension(client):
+	data = {}
+	data['file'] = (io.BytesIO(b"abcdef"), 'test.txt')
+	response = client.post('/pictures/save', data = data, content_type = 'multipart/form-data')
+	response = response.data.decode('utf8')
+	response = json.loads(response)
+	assert response["status"] == 0
+	assert "File extension not allowed" in response["msg"]
 
 def test_download(client):
 	response = client.get('/pictures/load?')
